9a268d921b1ee287e48547339183d8b9f13e8a51
GibsonCS <gibson.cruzds@gmail.com>
2025-12-06 22:21:59 -0300

    base project

---
+++ src/test/java/br/com/userauth/UserAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.userauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
