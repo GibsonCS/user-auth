132c688a4aa77f9e014a6dd393d385331f5204a4
GibsonCS <gibson.cruzds@gmail.com>
2025-12-11 00:29:57 -0300

    feat(application): modify register user

--- src/main/java/br/com/userauth/application/useCase/RegisterUser.java
+++ src/main/java/br/com/userauth/application/useCase/RegisterUser.java
@@ -1,18 +1,24 @@
 package br.com.userauth.application.useCase;
 
 import br.com.userauth.domain.exception.UserAlreadyExistException;
-import br.com.userauth.domain.model.User;
+import br.com.userauth.domain.model.role.Role;
+import br.com.userauth.domain.model.user.User;
+import br.com.userauth.domain.repository.RoleRepository;
 import br.com.userauth.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class RegisterUser {
 
+
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     public User execute(String login, String email, String password) {
 
@@ -22,11 +28,19 @@ public class RegisterUser {
             throw new UserAlreadyExistException("User already exist");
         }
 
+
+        Role role = roleRepository.findByName("USER");
+
+        Set<Role> roles = new HashSet<>();
+
+        roles.add(role);
+
         User user = new User(
                 UUID.randomUUID().toString(),
                 login,
                 email,
-                password
+                password,
+                roles
         );
 
         userRepository.save(user);
